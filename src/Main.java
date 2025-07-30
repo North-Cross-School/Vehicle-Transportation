521da3eae9d76ab1242405c9f2cdda866dac26a3
Tiffany Sakaguchi <82681691+tiffsaka@users.noreply.github.com>
2025-07-30 16:27:15 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: Prompt user for number of SUVs
+        // TODO: Prompt user for number of cars
+        // TODO: Calculate full carriers needed and leftovers
+        // TODO: Print results
+    }
+}
